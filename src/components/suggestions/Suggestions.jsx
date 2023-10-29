7c2bacffea62282cf3702aa5416facc91f03b4d0
quantum0X <thevishal2108@gmail.com>
2023-10-29 16:53:00 +0530

    artist section completed

--- src/components/suggestions/Suggestions.jsx
+++ src/components/suggestions/Suggestions.jsx
@@ -1,13 +1,78 @@
 import React from "react";
+import Artist from "./Artist";
+import {
+  Artist1,
+  Artist2,
+  Artist3,
+  UserDP1,
+  UserDP2,
+  UserDP3,
+} from "../../utils/ImageImport";
+
+const data = [
+  {
+    profile: UserDP1,
+    name: "Thomas Edward",
+    username: "thewildwithyou",
+    photo: Artist1,
+  },
+  {
+    profile: UserDP2,
+    name: "Thomas Edward",
+    username: "thewildwithyou",
+    photo: Artist2,
+  },
+  {
+    profile: UserDP3,
+    name: "Thomas Edward",
+    username: "thewildwithyou",
+    photo: Artist3,
+  },
+  {
+    profile: UserDP1,
+    name: "Thomas Edward",
+    username: "thewildwithyou",
+    photo: Artist2,
+  },
+  {
+    profile: UserDP1,
+    name: "Thomas Edward",
+    username: "thewildwithyou",
+    photo: Artist2,
+  },
+  {
+    profile: UserDP1,
+    name: "Thomas Edward",
+    username: "thewildwithyou",
+    photo: Artist2,
+  },
+];
 
 const Suggestions = () => {
   return (
-    <div>
+    <div className="h-full flex flex-col gap-8 ">
       {/* become a seller */}
       <div className="bg-sellerBtn px-12 py-5 text-white rounded-md">
         Become a Seller
       </div>
-      Suggestions
+      {/* other catagories */}
+      <div className="">
+        <div className="flex gap-6 font-semibold">
+          <span className="text-mainBlack">Artists</span>
+          <span className="text-midBlack">Photographers</span>
+        </div>
+        <div id="artist" className="h-[73vh] my-2 pr-2 overflow-y-scroll">
+          {data.map((ele, i) => (
+            <Artist key={i} {...ele} />
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-around text-xs">
+        <div>Privacy</div>
+        <div>Terms of Service</div>
+        <div>Cookie Notice</div>
+      </div>
     </div>
   );
 };
--- src/utils/ImageImport.js
+++ src/utils/ImageImport.js
@@ -1,4 +1,9 @@
 import UserDP1 from '../assets/user-dp-1.png'
+import UserDP2 from '../assets/user-dp-2.png'
+import UserDP3 from '../assets/user-dp-3.png'
 import Post1 from '../assets/user-post-1.png'
+import Artist1 from '../assets/artist-1.png'
+import Artist2 from '../assets/artist-2.png'
+import Artist3 from '../assets/artist-3.png'
 
-export { UserDP1, Post1 }
+export { UserDP1, UserDP2, UserDP3, Post1, Artist1, Artist2, Artist3 }
