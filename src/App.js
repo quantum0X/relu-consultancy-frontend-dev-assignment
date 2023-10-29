cedf0f2b8891408a2405c54af90ccfa046fe1eba
quantum0X <thevishal2108@gmail.com>
2023-10-29 11:52:54 +0530

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+import Feed from './components/feed/Feed';
+import Suggestions from './components/suggestions/Suggestions';
 
 function App() {
   return (
-    <div className="">
-      hello
+    <div className="h-screen max-w-6xl mx-auto pt-6 px-4 flex flex-col">
+      <Navbar />
+      <div>
+        <Sidebar />
+        <Feed />
+        <Suggestions />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sellerBtn: "#88C2BB",
+        mainBackground: "#000020",
+        mainBlack: "#101010",
+        midBlack: "#8D8D8D",
+        elementBg: "#FFFFFF"
+      }
+    },
   },
   plugins: [],
 }
